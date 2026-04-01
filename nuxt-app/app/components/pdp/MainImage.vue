1d78624eee653db63388b3a6b32e2495715f8b87
JustinGardners <justin.small@gardners.com>
2026-04-01 15:59:34 +0000

    added mainImage component for PDP

---
+++ nuxt-app/app/components/pdp/MainImage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+type Props = {
+  imageSrc: string;
+  productId: string;
+  title: string;
+  alt: string;
+}
+
+const { imageSrc, productId, title, alt } = defineProps<Props>()
+
+</script>
+
+<template>
+  <div v-if="imageSrc" class="imageWrap">
+    <div class="image">
+      <div class="jacket jacket-Book">
+        <img
+          :alt
+          class="productImage"
+          data-zoom-image="//tmg.dmmserver.com/media/640/97802417/9780241743980.jpg"
+          :id="`img-${productId}`"
+          :src="imageSrc"
+          :title
+        />
+      </div>
+      <span class="availabilityFlash inStock"></span>
+    </div>
+  </div>
+</template>
